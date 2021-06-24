7ff6e8920c9889c51e0585f1f83365fb7156575d
Ron McLaren <ronald.mclaren@noaa.gov>
2021-06-24 10:04:48 -0400

    Better example

--- main.f90
+++ main.f90
@@ -1,7 +1,7 @@
 module fort_test
 
   type, public :: InnerObj
-    real(kind=8), allocatable :: data(:)
+    real(kind=4), allocatable :: data(:)
   end type
 
   interface InnerObj
@@ -10,6 +10,7 @@ module fort_test
 
 
   type, public :: OuterObj
+    character(len=10) :: name
     type(InnerObj), allocatable :: innerObjs(:)
   end type
 
@@ -37,7 +38,7 @@ contains
 
 
   type(OuterObj) function init__outer_obj() result(obj)
-    obj = OuterObj(null()) ! needed due to gfortran issue
+    obj = OuterObj("ABCD", null()) ! needed due to gfortran issue
   end function init__outer_obj
 
 
@@ -70,11 +71,28 @@ program main
 
   type(MainObj) :: main_obj
   type(OuterObj) :: outer_obj
+  type(InnerObj) :: inner_obj1, inner_obj2
 
-  main_obj = MainObj()
+
+  inner_obj1 = InnerObj()
+  allocate(inner_obj1%data(0))
+  inner_obj1%data = [inner_obj1%data, 1.0]
+  inner_obj1%data = [inner_obj1%data, 2.0]
+  inner_obj1%data = [inner_obj1%data, 3.0]
+
+  inner_obj2 = InnerObj()
+  allocate(inner_obj2%data(0))
+  inner_obj2%data = [inner_obj2%data, 1.0]
+  inner_obj2%data = [inner_obj2%data, 2.0]
+  inner_obj2%data = [inner_obj2%data, 3.0]
 
   outer_obj = OuterObj()
-  call main_obj%add(outer_obj)
+  outer_obj%name = "SOMETHING"
+  allocate(outer_obj%innerObjs(0))
+  outer_obj%innerObjs = [outer_obj%innerObjs, inner_obj1]
+  outer_obj%innerObjs = [outer_obj%innerObjs, inner_obj2]
 
+  main_obj = MainObj()
+  call main_obj%add(outer_obj)
 
 end program main
